136e673beaf92fd3e4f2f91236ce90fe855e122b
Rodrigo Dantas <rodrigo.dantas@hustletech.dev>
2025-07-21 15:14:06 +0000

    Add CNAME file and update build configurations for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/hustletech-site/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
